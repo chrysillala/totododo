18a12786cb6e3fe27eedc038a02c7b4d7b5628f8
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-10-25 14:39:58 +0700

    add todo list conditional when empty

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,21 +1,23 @@
-const TodoList = ({ todos, priorities, onHandleCompleteTodo, onHandleRemoveTodo }) => (
-  <ul>
-    {todos.map((todo) => (
-      <li key={todo.id}>
-        <p style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
-          {todo.title}
-        </p>
-        {priorities[todo.priority]} - {(new Date(todo.dueDate)).toLocaleDateString()}
-        <br />
-        {todo.description}
-        <br />
-        {!todo.isCompleted &&
-          <button onClick={() => onHandleCompleteTodo(todo.id)}>Complete</button>
-        }
-        <button onClick={() => onHandleRemoveTodo(todo.id)}>Delete</button>
-      </li>
-    ))}
-  </ul>
-);
+const TodoList = ({ todos, priorities, onHandleCompleteTodo, onHandleRemoveTodo }) => {
+  return todos.length
+    ? (<ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>
+          <p style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+            {todo.title}
+          </p>
+          {priorities[todo.priority]} - {(new Date(todo.dueDate)).toLocaleDateString()}
+          <br />
+          {todo.description}
+          <br />
+          {!todo.isCompleted &&
+            <button onClick={() => onHandleCompleteTodo(todo.id)}>Complete</button>
+          }
+          <button onClick={() => onHandleRemoveTodo(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>)
+    : (<p>no todo</p>)
+};
 
 export default TodoList;
