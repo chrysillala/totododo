e5e27e815971d6fea86a62aa0bfce13c9fc4bf71
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-11-02 19:08:59 +0700

    update TodoContainer style

--- src/components/TodoContainer/index.js
+++ src/components/TodoContainer/index.js
@@ -3,11 +3,12 @@ import styled from 'styled-components';
 const StyledContainer = styled.div`
   display: grid;
   gap: 1rem;
-  grid-template-columns: minmax(100px, 40%) 1fr;
+  grid-template-columns: minmax(100px, 40%) minmax(300px, 60%);
   background-color: #083358;
   border-radius: 20px;
   overflow: hidden;
   height: 700px;
+  width: 960px;
 `
 
 const TodoContainer = ({ children }) => (
