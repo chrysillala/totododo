6d0fa6f9d6421fa7bc5e8552afbff78116b2658e
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-11-08 01:35:06 +0700

    remove uuid import from App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { v4 as uuidv4 } from 'uuid';
-
 import Layout from './components/Layout';
 import TodoForm from './components/TodoForm';
 import TodoSearch from './components/TodoSearch';
