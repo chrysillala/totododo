69a3a592daef6230bc0361b8fd5f22bd8c715bb5
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-11-10 18:26:31 +0700

    refactor Account component

---
+++ src/components/Account/Account.styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const UserPanelWrapper = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  color: var(--white);
+`;
