61d255c9d9f661ddf5cd1bfb058e1fec5a6a5612
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2021-11-10 18:01:08 +0700

    add Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+import Auth from '../components/Auth';
+
+const Login = () => {
+  return (
+    <Layout>
+      <Auth />
+    </Layout>
+  )
+}
+
+export default Login
